54c620c80e035ca3cb2de5637a11c9624d516dad
Valerii from earth <lvaldev.code@gmail.com>
2022-09-04 16:23:14 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
 import "./App.css";
+import Legend from "./components/Legend";
+import PieChart from "./components/PieChart";
 
 function App() {
-	return <div className='App'>start</div>;
+	const style = {};
+	return (
+		<div className='App'>
+			<PieChart style={style} />
+			<Legend />
+			<div className='data'>
+				<div className='input'>
+					<label for='1'>1</label> <input id='1' />
+				</div>
+				<div className='input'>
+					<label for='2'>2</label> <input id='2' />
+				</div>
+				<div className='input'>
+					<label for='3'>3</label> <input id='3' />
+				</div>
+				<div className='input'>
+					<label for='4'>4</label> <input id='4' />
+				</div>
+				<div className='input'>
+					<label for='5'>5</label> <input id='5' />
+				</div>
+				<div className='input'>
+					<label for='6'>6</label> <input id='6' />
+				</div>
+				<div className='input'>
+					<label for='7'>7</label> <input id='7' />
+				</div>
+				<div className='input'>
+					<label for='8'>8</label> <input id='8' />
+				</div>
+				<div className='input'>
+					<label for='9'>9</label> <input id='9' />
+				</div>
+				<div className='input'>
+					<label for='10'>10</label> <input id='10' />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/PieChart.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function PieChart() {
+	return (
+		<div className='wrap'>
+			<div className='pie pie_1'></div>
+			<div className='pie pie_2'></div>
+			<div className='pie pie_3'></div>
+			<div className='pie pie_4'></div>
+			<div className='pie pie_5'></div>
+			<div className='pie pie_6'></div>
+			<div className='pie pie_7'></div>
+			<div className='pie pie_8'></div>
+			<div className='pie pie_9'></div>
+			<div className='pie pie_10'></div>
+		</div>
+	);
+}
